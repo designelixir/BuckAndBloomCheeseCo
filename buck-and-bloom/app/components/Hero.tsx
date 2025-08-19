821a134d4bc7bb2b8bf0e22cce3f4e16b19102dd
Megan Byers <megan@designelixir.studio>
2025-08-19 15:48:55 -0600

    initial commit

--- buck-and-bloom/app/components/Hero.tsx
+++ buck-and-bloom/app/components/Hero.tsx
@@ -3,20 +3,29 @@ import { useEffect, useMemo, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
+import Nav from "./Nav";
 
 export default function Hero() {
+    
+
 
     return(
-        <div className="flex-center-center flex-column hero-container">
-            {/* <h1 className="centered-text">Welcome!<br></br>We're is a small but mighty team of cheesemakers offering artisan goat cheeses to our home state of Colorado.</h1> */}
-            
-            <div className="mountains">
-                <div className="flex-center-center logo-container">
+        <div className="hero">
+            <div className="sunburst-wrapper flex-center-center full-width">
+                    <div className="sunburst scaledown-sunburst"></div>
+                    <div className="flex-center-center logo-banner-container logo-enter">
                     <Image width={500} height={220} src="/BuckBloom-Tagline.svg" alt="Buck and Bloom Logo"></Image>
                 </div>
             </div>
+        <div className="flex-center-center flex-column hero-container">
             
             
+            <div className="mountains flex-center-center mtns-enter">
+                <Nav></Nav>
+            </div>
+            
+            
+        </div>
         </div>
     )
 }
---
+++ buck-and-bloom/app/components/Nav.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useEffect, useMemo, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+
+export default function Nav() {
+    
+
+
+    return(
+        <nav>
+            
+        </nav>
+    )
+}
